7ee0a38cd8a45780606a7c7e38ab49fd298bd683
mehedihasanshipon <siponmahmud112255@gmail.com>
2021-03-20 13:37:36 +0600

    React-auth-assignment

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Navbar,Nav} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import logo from '../../images/logo.png';
 import './Header.css'
 
 
 const Header = () => {
+    const {type} =useParams();
+    console.log(type);
+  
     return (
         <div>
             <Navbar className="navBar" collapseOnSelect expand="lg" >
@@ -15,7 +18,7 @@ const Header = () => {
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="ms-auto">
                 <Link className="nav-link" to="/">Home</Link>
-                <Link className="nav-link" to="/ride/bike">Destination</Link>
+                <Link className="nav-link" to={`/ride/${type || 'bike'}`}>Destination</Link>
                 <Link className="nav-link" to="#">Blog</Link>
                 <Link className="nav-link" to="/contact">Contact</Link>
                 <Link className="btn btn-danger " to="/login"><span className="nav-button">Log in</span></Link>
